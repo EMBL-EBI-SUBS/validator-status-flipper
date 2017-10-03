c9555ff4e64a242bf8c971e4e3435bbd3c2697c6
Karoly Erdos <karoly@ebi.ac.uk>
2017-10-03 13:49:39 +0100

    Remove unneeded Jackson2JsonMessageConverter

--- src/main/java/uk/ac/ebi/subs/validator/flipper/messaging/StatusFlipperMessagingConfiguration.java
+++ src/main/java/uk/ac/ebi/subs/validator/flipper/messaging/StatusFlipperMessagingConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -22,16 +21,6 @@ import static uk.ac.ebi.subs.validator.flipper.messaging.StatusFlipperRoutingKey
 @Configuration
 @ComponentScan(basePackageClasses = ExchangeConfig.class)
 public class StatusFlipperMessagingConfiguration {
-
-    /**
-     * Instantiate a JSON message converter.
-     *
-     * @return an instance of JSON message converter.
-     */
-    @Bean
-    public Jackson2JsonMessageConverter messageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
 
     /**
      * Instantiate a {@link Queue} for publish events related to the validation result document.
